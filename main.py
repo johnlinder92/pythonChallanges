b3988787c7b6c75eca4ccb63a940a08fff14d8ca
john <john.artur.linder@gmail.com>
2021-07-28 14:40:17 +0200

    test

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Skift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#hmm
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def flatten(listoflists):
+    result = []
+    for i in listoflists:
+        for x in i:
+            result.append(x)
+    return result
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
